572f23cae4cc5656e65baf9e3063a22900bb66cf
swb <1624861606@qq.com>
2020-06-06 11:48:19 +0800

    [add]http NewRequest do

--- httpDo.go
+++ httpDo.go
@@ -1,1 +1,27 @@
 package process
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func HttpJsonDo(url string, jsonMarshal []byte, httpMethod string) ([]byte, error) {
+	var err error
+	var response []byte
+	client := &http.Client{}
+	req, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(jsonMarshal))
+	if err != nil {
+		return response, err
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	resp, err := client.Do(req)
+	if err != nil {
+		return response, err
+	}
+	response, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return response, err
+	}
+	return response, nil
+}
