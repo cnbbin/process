58df94ca1bd4a75ed0ad7cd2225bba70ad106868
swb <1624861606@qq.com>
2020-06-10 09:55:12 +0800

    [add]十进制计算二进制 0 1 个数 与转换为2进制 (面试常用之不能用自带二进制SDK)

---
+++ convertToBin.go
@@ -0,0 +1,34 @@
+package process
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//	@convertToBin
+// 	@description  	DecimalNumberToBin
+// 	@Author 		CyBen
+// 	@param   		DecimalNumber 		int
+//	@return   		BinNumber   Bin1count Bin0count		string int int
+func convertToBin(num int) (string, int, int) {
+	s := ""
+	a := 0
+	c := 0
+	if num == 0 {
+		return "0", 0, 1
+	}
+
+	// num /= 2 每次循环的时候 都将num除以2  再把结果赋值给 num
+	for ; num > 0; num /= 2 {
+		lsb := num % 2
+		fmt.Println(lsb)
+		if lsb == 1 {
+			a++
+		} else {
+			c++
+		}
+		// strconv.Itoa() 将数字强制性转化为字符串
+		s = strconv.Itoa(lsb) + s
+	}
+	return s, a, c
+}
